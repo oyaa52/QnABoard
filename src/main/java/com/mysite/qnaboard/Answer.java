21fade36ba480b69664c7fcfb3080b976644155a
yeonjilim <imyeonji520@gmail.com>
2024-12-14 18:55:02 +0900

    (2-04) Setting Entities - Answer and Question

---
+++ src/main/java/com/mysite/qnaboard/Answer.java
@@ -0,0 +1,24 @@
+package com.mysite.qnaboard;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
