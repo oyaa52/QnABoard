a39a11292351762d8d135ffc7863679ef2f3fb20
yeonjilim <imyeonji520@gmail.com>
2024-12-14 18:30:43 +0900

    Setting

---
+++ src/test/java/com/mysite/qnaboard/QnABoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.qnaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnABoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
